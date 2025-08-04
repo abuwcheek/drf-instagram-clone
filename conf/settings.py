fab125ff8f5c7846168e1e385a654ab08f14d446
abuwcheek <abdulloistamov1034@gmail.com>
2025-08-04 19:03:20 +0500

    delete profile otpravlen 6ssifr kod

--- conf/settings.py
+++ conf/settings.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 import os
+from environs import Env
+env = Env()
+# Load environment variables from a .env file
+env.read_env()
+
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -10,10 +16,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4)64%s3m04uqv6bu=w5j%sbv9xdm(m4heuikh*(bsj4zlzltic'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG")
+
 
 ALLOWED_HOSTS = ['*']
 
@@ -162,9 +169,11 @@ AUTH_USER_MODEL = 'users.User'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'abdulloistamov1034@gmail.com'
+DEFAULT_FROM_EMAIL = env.str('DEFAULT_FROM_EMAIL')
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'abdulloistamov1034@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_USER_SSL = False
