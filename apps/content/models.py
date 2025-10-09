6f2cf354d65f8c9506e6fa6c324ce320d61a4444
abuwcheek <abdulloistamov1034@gmail.com>
2025-10-09 23:01:10 +0500

    content/models.py post, postfile modellari boldi

--- apps/content/models.py
+++ apps/content/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-# from users.models import User
-from base.models import BaseModel
+from django.core.validators import FileExtensionValidator
+from apps.base.models import BaseModel
+from apps.users.models import User
 from .shortid import generate_shortcode
 
 
@@ -9,9 +10,6 @@ from .shortid import generate_shortcode
 
 class Tag(BaseModel):
      name = models.CharField(max_length=50, unique=True)
-     
-     class Meta:
-          abstract = True
 
      def __str__(self):
           return self.name
@@ -20,7 +18,7 @@ class Tag(BaseModel):
 
 class Post(BaseModel):
      short_id = models.CharField(max_length=20, unique=True, blank=True, null=True)
-     user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='posts')
+     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
      caption = models.TextField(blank=True, null=True)
      location = models.CharField(max_length=255, blank=True, null=True)
      tags = models.ManyToManyField(Tag, null=True, blank=True, related_name='tags')
@@ -42,14 +40,7 @@ class Post(BaseModel):
 
 class PostFiles(BaseModel):
      post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='files')
-     file = models.FileField(upload_to='post/media/')
-     file_type = models.CharField(max_length=10, choices=[('image', 'Image'),
-                                                       ('jpeg', 'Jpeg'),
-                                                       ('img', 'Img'),
-                                                       ('heic', 'Heic'), 
-                                                       ('video', 'Video'),
-                                                       ('mp4', 'Mp4'),
-                                                       ('mov', 'Mov')])
+     file = models.FileField(upload_to='post/media/', validators=[FileExtensionValidator(allowed_extensions=('jpg', 'jpeg', 'png', 'heic', 'hd', 'img', 'mp4', 'mov'))])
      count = models.PositiveIntegerField(default=0)
      order = models.PositiveIntegerField(default=0)
 
